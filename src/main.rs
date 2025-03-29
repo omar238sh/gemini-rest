e8f1b381e4c6035ac6b51b59bb83853ce96ac846
Omar Sherif <omar238sh@gmail.com>
2025-03-29 18:09:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use dotenv::dotenv;
 use std::env;
-
+mod cli;
 use aii::ask;
 use aii::print_response;
-
+use clap::Parser;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
+    let args = cli::AskCli::parse();
     let key = match env::var("key") {
         Ok(key) => key,
         Err(e) => panic!("Failed to get key in envfile: {}", e),
     };
 
-    let response = ask(&key, "Hello gemini How are you").await.unwrap();
+    let response = ask(&key, &args.question).await.unwrap();
+
     print_response(response).await.unwrap();
 }
